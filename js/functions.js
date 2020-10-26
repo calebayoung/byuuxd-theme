4a599e52bc293ce04731a3ddc7416e0d54a4da66
Caleb Young <caleb.a.young@gmail.com>
2020-10-25 18:03:10 -0600

    Misc. cleanup, set up ESLint

--- js/functions.js
+++ js/functions.js
@@ -1,16 +1,17 @@
-jQuery( $ => {
+/* global jQuery */
+jQuery( ( $ ) => {
 	$( () => {
 		$( '.uxd-header__menu-icon' ).on( 'click', () => {
-			let mobileNav = $( '.uxd-header__mobile-nav' ),
+			const mobileNav = $( '.uxd-header__mobile-nav' ),
 				openIcon = $( '.uxd-header__menu-icon--open' ),
 				closeIcon = $( '.uxd-header__menu-icon--close' );
 			openIcon.toggleClass( 'hidden' );
 			closeIcon.toggleClass( 'hidden' );
 			if ( 0 === mobileNav.height() ) {
-				mobileNav.height( mobileNav[0].scrollHeight );
+				mobileNav.height( mobileNav[ 0 ].scrollHeight );
 			} else {
 				mobileNav.height( 0 );
 			}
-		});
-	});
-});
+		} );
+	} );
+} );
